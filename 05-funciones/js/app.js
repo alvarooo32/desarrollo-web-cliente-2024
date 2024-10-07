979e7a3b64ba246e5081689b7704107ea9d39822
alvarooo32 <alvaro23.ossa@gmail.com>
2024-10-07 11:29:15 +0200

    nueva actu

--- 05-funciones/js/app.js
+++ 05-funciones/js/app.js
@@ -5,9 +5,18 @@ let resultado = func(10,10);
 console.log(resultado);
 
 
-
 //funcion autoinvocadas
 (function(){
     let msg = "hola!";
     console.log(msg);
 })();
+
+//función anonima
+//let a = 10;
+//let b = 10;
+resultado = function(a,b){return a*b};
+console.log(resultado(10,10));
+
+//Notacion flecha -- arrow functions
+const arrowF = (a,b) => a*b;
+console.log(arrowF(10,10));
